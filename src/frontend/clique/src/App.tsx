294a28b1d3622787a05139dab7aa0da1f507dfbf
lily-broman <lilybroman25@outlook.com>
2025-03-01 19:28:27 -0600

    Added a couple MUI design components to use as we develop and played around with some components (for learning purposes)

--- src/frontend/clique/src/App.tsx
+++ src/frontend/clique/src/App.tsx
@@ -1,11 +1,21 @@
 import './App.css'
 import SideBar from './components/sidebar'
+import { Container, Typography } from "@mui/material";
 
 function App() {
   return (
     <>
-    <SideBar/>
-      <h1>Clique</h1>
+    <Container>
+
+      <SideBar/>
+      {/* Below is an example strcture - Lily */}
+      <Typography 
+        variant='h1' 
+        sx={{}}>
+        Clique
+      </Typography>
+
+    </Container>
     </>
   )
 }
--- src/frontend/clique/src/main.tsx
+++ src/frontend/clique/src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ThemeProvider } from '@emotion/react'
+import { createTheme } from '@mui/material'
+
+//This is where we will create/declare our style theme
+const theme = createTheme()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
