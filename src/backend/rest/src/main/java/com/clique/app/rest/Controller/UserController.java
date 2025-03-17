81b7c930c1c823c37e4aa29d1b89ba4c568a65d4
lily-broman <lilybroman25@outlook.com>
2025-03-17 17:41:47 -0500

    Fetching data trial

--- src/backend/rest/src/main/java/com/clique/app/rest/Controller/UserController.java
+++ src/backend/rest/src/main/java/com/clique/app/rest/Controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173") //Allow frontend to access backend
 @RequestMapping("/users")
 public class UserController {
 
