b70d0dd024ef741ffb50c889347c241416dd54cf
Lily Broman <137229876+lily-broman@users.noreply.github.com>
2025-04-16 10:56:37 -0500

    Deleting duplicate files
    Last contributor uploaded the entire project a second time, breaking the entire project, instead of just the files that they edited.

--- UserHub/Clique App/Clique-main/src/backend/rest/src/main/java/com/clique/app/rest/Repository/PostRepo.java
+++
@@ -1,9 +0,0 @@
-package com.clique.app.rest.Repository;
-
-import com.clique.app.rest.Models.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostRepo extends JpaRepository<Post, Long> {
-}
