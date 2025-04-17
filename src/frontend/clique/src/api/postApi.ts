ed7cc28ee6387672829aeda65fc92b831067252b
lexiestar <lexiestar24@gmail.com>
2025-04-16 20:38:11 -0500

    display card feed

--- src/frontend/clique/src/api/postApi.ts
+++ src/frontend/clique/src/api/postApi.ts
@@ -1,20 +1,22 @@
 import { makeRequest } from "./apiService";
-import { ApiResponse, Post, PostCreationRequest } from "../types/postTypes";
+import {
+  AllPosts,
+  ApiResponse,
+  Post,
+  PostCreationRequest,
+} from "../types/postTypes";
 
 //FETCH: all posts
-export const getPosts = (): Promise<ApiResponse<Post>> => {
-return makeRequest<Post>(`posts`, 'GET');
+export const getPosts = (): Promise<ApiResponse<AllPosts[]>> => {
+  return makeRequest<AllPosts[]>(`posts`, "GET");
 };
 
 //FETCH: a single post by ID
-export const getPostById = (
-  id: Number
-): Promise<ApiResponse<Post>> => {
-
-if (!id) {
-  throw new Error('No id provided')
-}
-return makeRequest<Post>(`posts/${id}`, 'GET');
+export const getPostById = (id: Number): Promise<ApiResponse<Post>> => {
+  if (!id) {
+    throw new Error("No id provided");
+  }
+  return makeRequest<Post>(`posts/${id}`, "GET");
 };
 
 //CREATE: new post
@@ -22,10 +24,12 @@ export const createPost = (
   postData: Partial<PostCreationRequest>
 ): Promise<ApiResponse<Post>> => {
   if (!postData) {
-    throw new Error('Post data is required to CREATE a new post. Please enter valid post data.');
+    throw new Error(
+      "Post data is required to CREATE a new post. Please enter valid post data."
+    );
   }
 
-  return makeRequest<Post>('posts/save', 'POST', postData);
+  return makeRequest<Post>("posts/save", "POST", postData);
 };
 
 //UPDATE: exisitng post
--- src/frontend/clique/src/pages/Feed.tsx
+++ src/frontend/clique/src/pages/Feed.tsx
@@ -1,9 +1,47 @@
+import { useEffect, useState } from "react";
+import FeedCard from "../components/FeedCard";
 import PageWrapper from "./PageWrapper";
+import { getPosts } from "../api/postApi";
+import { AllPosts } from "../types/postTypes";
+import { Box } from "@mui/material";
 
 const Feed = () => {
+  const [posts, setPosts] = useState<AllPosts[]>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await getPosts();
+        if (!response) throw new Error(`HTTP error!`);
+        console.log(response);
+        setPosts(response.data);
+      } catch (error) {
+        console.error("Failed to fetch data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(posts);
+
   return (
     <PageWrapper>
       <h1>Feed</h1>
+      {posts?.map((item, index) => {
+        return (
+          <Box sx={{ padding: 2 }}>
+            <FeedCard
+              key={index}
+              username={item.author.username}
+              date={item.createdAt}
+              content={item.caption}
+              location={item.location}
+              likeCount={item.likeCount}
+            />
+          </Box>
+        );
+      })}
     </PageWrapper>
   );
 };
