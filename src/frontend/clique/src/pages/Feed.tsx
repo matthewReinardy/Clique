814f7536f00514e51a7b91c3cf4ab712a406989d
lexiestar <lexiestar24@gmail.com>
2025-04-16 20:46:47 -0500

    Update Feed.tsx

--- src/frontend/clique/src/pages/Feed.tsx
+++ src/frontend/clique/src/pages/Feed.tsx
@@ -7,41 +7,46 @@ import { Box } from "@mui/material";
 
 const Feed = () => {
   const [posts, setPosts] = useState<AllPosts[]>();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
+      setLoading(true);
       try {
         const response = await getPosts();
         if (!response) throw new Error(`HTTP error!`);
-        console.log(response);
+        setLoading(false);
         setPosts(response.data);
       } catch (error) {
         console.error("Failed to fetch data:", error);
+        setLoading(false);
       }
     };
 
     fetchData();
   }, []);
 
-  console.log(posts);
-
   return (
     <PageWrapper>
       <h1>Feed</h1>
-      {posts?.map((item, index) => {
-        return (
-          <Box sx={{ padding: 2 }}>
-            <FeedCard
-              key={index}
-              username={item.author.username}
-              date={item.createdAt}
-              content={item.caption}
-              location={item.location}
-              likeCount={item.likeCount}
-            />
-          </Box>
-        );
-      })}
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        posts?.map((item, index) => {
+          return (
+            <Box sx={{ padding: 2 }}>
+              <FeedCard
+                key={index}
+                username={item.author.username}
+                date={item.createdAt}
+                content={item.caption}
+                location={item.location}
+                likeCount={item.likeCount}
+              />
+            </Box>
+          );
+        })
+      )}
     </PageWrapper>
   );
 };
