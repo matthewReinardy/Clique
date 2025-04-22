8b0a565d112d813c7f849654f36dfa26635ca8c9
BJarvi05 <nr1821dh@go.minnstate.edu>
2025-04-22 02:12:41 -0500

    added the ability to see ads within the feed if following a business account

--- src/backend/rest/src/main/java/com/clique/app/rest/Controller/FeedController.java
+++ src/backend/rest/src/main/java/com/clique/app/rest/Controller/FeedController.java
@@ -3,6 +3,8 @@ package com.clique.app.rest.Controller;
 import com.clique.app.rest.Controller.DTOs.PostDTO;
 import com.clique.app.rest.Models.Post;
 import com.clique.app.rest.Models.User;
+import com.clique.app.rest.Models.Ad;
+import com.clique.app.rest.Repo.AdRepo;
 import com.clique.app.rest.Repo.PostRepo;
 import com.clique.app.rest.Repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +31,10 @@ public class FeedController {
     @Autowired
     private PostRepo postRepo;
 
+    @Autowired
+    private AdRepo adRepo;
+
+
     // Endpoint for getting a feed for a user, the user you pass in will return the feed for who they follow
     @GetMapping("/{userId}")
     public ResponseEntity<?> getFeed(@PathVariable Long userId) {
@@ -50,14 +56,34 @@ public class FeedController {
             // Storing them in a list since there can be duplicates
 
             List<Post> feedPosts = new ArrayList<>();
+            List<PostDTO> feedDTOs = new ArrayList<>();
             for (User followedUser : following) {
                 List<Post> userPosts = postRepo.findByAuthor(followedUser);
                 feedPosts.addAll(userPosts);
+
+                // Check if the followed user is a business account, and show ads if they are
+                if ("Business".equalsIgnoreCase(followedUser.getAccountType())) {
+                    List<Ad> ads = adRepo.findByUser(followedUser);
+                    if (ads != null) {
+                        for (Ad ad : ads) {
+                                PostDTO adDto = new PostDTO(
+                                        ad.getId(),
+                                        "Sponsored", // caption for all ads
+                                        null,         // null location
+                                        null,         // null tag
+                                        "",           // empty creation date
+                                        ad.getUser().getUsername(),
+                                        0,            // default likes and comments
+                                        0,
+                                        ad.getImage()
+                                );
+                                feedDTOs.add(adDto); // Add the ad to the feedDTOs list
+
+                        }
+                    }
+                }
             }
-
             // Convert to DTOs
-            List<PostDTO> feedDTOs = new ArrayList<>();
-
             for (Post post : feedPosts) {
                 PostDTO dto = new PostDTO(
                         post.getId(),
