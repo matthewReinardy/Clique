b673803fbdc78d2b81a3065755b4a2c9cbdd3a99
Abdilahiy25 <hi8715ku@go.minnstate.edu>
2025-04-22 07:23:08 -0500

    create post and delete post

--- Clique-Create-Post-Component-FE-2 (11)/Clique-Create-Post-Component-FE-2/src/backend/rest/src/main/java/com/clique/app/rest/Repo/UserLikeRepo.java
+++
@@ -1,7 +0,0 @@
-package com.clique.app.rest.Repo;
-
-import com.clique.app.rest.Models.UserLike;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserLikeRepo extends JpaRepository<UserLike, Long> {
-}
